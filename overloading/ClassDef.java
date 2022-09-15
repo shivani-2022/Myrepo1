c06f518902cc935b28e1656749e8c4420f217065
shivani-2022 <shivanidingore0229@gmail.com>
2022-09-15 17:53:56 +0530

    My first commit

---
+++ overloading/ClassDef.java
@@ -0,0 +1,14 @@
+package overloading;
+
+public class ClassDef {
+
+	/**
+	 * Class is collection of statements
+	 * 							- Identifier
+	 * 							- Literals
+	 * 							- Keyword
+	 * Collection of Members
+	 * 				- static member (variable & method)
+	 * 				- non static member (variable & method)
+	 */
+}
---
+++ overloading/MethodOverloading7.java
@@ -0,0 +1,23 @@
+package overloading;
+/**
+ * Question 2 – return type is different. Method name & argument list same.
+ */
+public class MethodOverloading7
+{
+   public double myMethod1(int num1, int num2)
+   {
+      System.out.println("First myMethod of class Demo");
+      return num1+num2;
+   }
+   public int myMethod(int var1, int var2)
+   {
+      System.out.println("Second myMethod of class Demo");
+      return var1-var2;
+   }
+   public static void main(String args[])
+   {
+      MethodOverloading7 obj2= new MethodOverloading7();
+      obj2.myMethod1(10,10);
+      obj2.myMethod1(20,12);
+   }
+}
